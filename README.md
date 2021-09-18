8f903782da248c6e81dc711c932a4a2164f71fd6
Jon Leech <oddhack@sonic.net>
2021-09-17 19:33:23 -0700

    Initial framework for glTF Registry repository.
    To be populated with glTF 2.0 specification outputs once approved by the
    glTF Working Group.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # glTF-Registry
-glTF specification registry and backing store for www.khronos.org/registry/glTF
+
+**NOTE** this registry is not yet official, and is published solely to
+collect feedback from glTF Working Group members.
+
+The glTF-Registry repository contains the glTF 2.0 Specification, as well as
+links to older specifications and extensions in Github.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/glTF/ ; commits to the main branch of this
+repository will be reflected there.
+
+Interesting files in this repository include:
+
+* index.php - toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* specs/ - glTF specification documents.
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$static_title = 'Khronos glTF Registry';
+
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<p> The glTF registry contains the specification of the Khronos Neural
+    Network Exchange Format. </p>
+
+<h2> <a name="specs"></a> <b>glTF Specification</b> </h2>
+
+<p> The current version of glTF is 2.0: </p>
+
+<ul>
+<li> glTF 2.0 in
+     <a href="specs/2.0/glTF.2.0.pdf">PDF</a> and
+     <a href="specs/2.0/glTF.2.0.html">HTML</a>. </li>
+</ul>
+
+<p> Previous versions of glTF: </p>
+
+<ul>
+<li> glTF 1.0 Specification in
+     <a href="https://github.com/KhronosGroup/glTF/tree/master/specification/1.0">
+     Markdown</a> (rendered by Github). </li>
+</ul>
+
+<h2 id="extensions"> <b>glTF Extension Registry</b> </h2>
+
+<a href="https://github.com/KhronosGroup/glTF/blob/master/extensions">
+Extensions for glTF 2.0</a> are in Markdown format and rendered by Github.
+
+<h2> <a name="feedback"></a> Providing Feedback on the Registry and Specification </h2>
+
+<p> Khronos welcomes comments and issue reports. </p>
+
+<p> To provide feedback <strong>on the glTF Specification</strong>, file an
+    issue in the <a href="https://github.com/KhronosGroup/glTF/issues/new">
+    KhronosGroup/glTF </a> Github project. </p>
+
+<p> To provide feedback <strong>on the glTF registry itself</strong> (such
+    as reporting missing content, bad links, etc.), file an issue in the <a
+    href="https://github.com/KhronosGroup/glTF-Registry/issues/new">
+    glTF-Registry </a> Github project. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
