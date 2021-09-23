24fd81ae013f12d43bac5a327c757cb07706d12b
Alexey Knyazev <3479527+lexaknyazev@users.noreply.github.com>
2021-09-23 16:34:27 +0400

    glTF 2.0.0

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # glTF-Registry
 
-**NOTE** this registry is not yet official, and is published solely to
-collect feedback from glTF Working Group members.
-
 The glTF-Registry repository contains the glTF 2.0 Specification, as well as
-links to older specifications and extensions in Github.
+links to older specifications and extensions in GitHub.
 
 It is also used as a backing store for the web view of the registry at
-https://www.khronos.org/registry/glTF/ ; commits to the main branch of this
+https://www.khronos.org/registry/glTF/. Commits to the main branch of this
 repository will be reflected there.
 
 Interesting files in this repository include:
 
-* index.php - toplevel index page for the web view. This relies on PHP
+* `index.php` - toplevel index page for the web view. This relies on PHP
   include files found elsewhere on www.khronos.org and so is not very useful
   in isolation.
-* specs/ - glTF specification documents.
+* `specs/` - glTF specification documents.
--- index.php
+++ index.php
@@ -4,8 +4,7 @@ $static_title = 'Khronos glTF Registry';
 include_once("../../assets/static_pages/khr_page_top.php");
 ?>
 
-<p> The glTF registry contains the specification of the Khronos GL
-    Transmission Format. </p>
+<p> The glTF registry contains the specification of the glTF — runtime 3D asset delivery format. </p>
 
 <h2> <a name="specs"></a> <b>glTF Specification</b> </h2>
 
@@ -13,27 +12,27 @@ include_once("../../assets/static_pages/khr_page_top.php");
 
 <ul>
 <li> glTF 2.0 in
-     <a href="specs/2.0/glTF.2.0.pdf">PDF</a> and
-     <a href="specs/2.0/glTF.2.0.html">HTML</a>. </li>
+     <a href="specs/2.0/glTF-2.0.pdf">PDF</a> and
+     <a href="specs/2.0/glTF-2.0.html">HTML</a>. </li>
 </ul>
 
-<p> The asciidoctor source of the specification is found in the
+<p> The AsciiDoc source of the specification is found in the
     <a href="https://github.com/KhronosGroup/glTF"> glTF specification
     repository</a>. Changes to the specification should be proposed in
-    Github issues or pull requests in that repository. </p>
+    GitHub issues or pull requests in that repository. </p>
 
 <p> Previous versions of glTF: </p>
 
 <ul>
 <li> glTF 1.0 Specification in
-     <a href="https://github.com/KhronosGroup/glTF/tree/master/specification/1.0">
-     Markdown</a> (rendered by Github). </li>
+     <a href="https://github.com/KhronosGroup/glTF/tree/main/specification/1.0">
+     Markdown</a> (rendered by GitHub). </li>
 </ul>
 
 <h2 id="extensions"> <b>glTF Extension Registry</b> </h2>
 
-<a href="https://github.com/KhronosGroup/glTF/blob/master/extensions">
-Extensions for glTF 2.0</a> are in Markdown format (rendered by Github).
+<a href="https://github.com/KhronosGroup/glTF/tree/main/extensions">
+Extensions for glTF 2.0</a> are in Markdown format (rendered by GitHub).
 
 <h2> <a name="feedback"></a> Providing Feedback on the Registry and Specification </h2>
 
@@ -41,12 +40,12 @@ Extensions for glTF 2.0</a> are in Markdown format (rendered by Github).
 
 <p> To provide feedback <strong>on the glTF Specification</strong>, file an
     issue in the <a href="https://github.com/KhronosGroup/glTF/issues/new">
-    KhronosGroup/glTF </a> Github project. </p>
+    KhronosGroup/glTF </a> GitHub project. </p>
 
 <p> To provide feedback <strong>on the glTF registry itself</strong> (such
     as reporting missing content, bad links, etc.), file an issue in the <a
     href="https://github.com/KhronosGroup/glTF-Registry/issues/new">
-    glTF-Registry </a> Github project. </p>
+    glTF-Registry </a> GitHub project. </p>
 
 <?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
 </body>
